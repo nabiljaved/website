2b9bd6f3ef4b7b2cf54d01a5ae0df2fb26dd4290
nabiljaved <nabeeljaved2016@gmail.com>
2023-02-02 11:03:22 +0400

    team section left

---
+++ assets/js/whatsup.js
@@ -0,0 +1,27 @@
+  
+  const whatsupA =  document.querySelector(".whatsup a");
+  const whatsup = document.querySelector(".whatsup");
+  const whatsupoffers = document.querySelector(".whatsup_buttons a");
+ 
+
+  var x = window.matchMedia("(max-width: 700px)");
+  
+  whatsup.addEventListener("click", function(){
+      
+  })
+
+  function myFunction(x) {
+    if (x.matches) { // If media query matches
+      whatsupA.href = "https://api.whatsapp.com/send?phone=+971564013448";
+      whatsupoffers.href = "https://api.whatsapp.com/send?phone=+971564013448";
+      console.log(whatsupoffers.href)
+    } else {
+      // document.body.style.backgroundColor = "pink";
+      whatsupA.href = "https://web.whatsapp.com/send?phone=+971564013448";
+      whatsupoffers.href = "https://web.whatsapp.com/send?phone=+971564013448";
+      console.log(whatsupoffers.href)
+    }
+  }
+  
+  
+  myFunction(x) 
