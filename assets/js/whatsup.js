5aa63b57c5dbd1e632af41e5af0bd2d230367208
nabiljaved <nabeeljaved2016@gmail.com>
2023-02-06 12:32:15 +0400

    added whatsup

--- assets/js/whatsup.js
+++ assets/js/whatsup.js
@@ -1,7 +1,7 @@
   
   const whatsupA =  document.querySelector(".whatsup a");
   const whatsupoffers = document.querySelector(".whatsup_buttons a");
-  var x = window.matchMedia("(max-width: 700px)");
+  var x = window.matchMedia("(max-width: 786px)");
   //console.log(whatsupoffers.href)
 
 
@@ -9,12 +9,12 @@
     if (x.matches) { // If media query matches
       whatsupA.href = "https://api.whatsapp.com/send?phone=+971564013448";
       whatsupoffers.href = "https://api.whatsapp.com/send?phone=+971564013448";
-      console.log(whatsupoffers.href)
+      // console.log(whatsupoffers.href)
     } else {
-      // document.body.style.backgroundColor = "pink";
-      whatsupA.href = "https://web.whatsapp.com/send?phone=+971564013448";
-      whatsupoffers.href = "https://web.whatsapp.com/send?phone=+971564013448";
-      console.log(whatsupoffers.href)
+      // // document.body.style.backgroundColor = "pink";
+      // whatsupA.href = "https://web.whatsapp.com/send?phone=+971564013448";
+      // whatsupoffers.href = "https://web.whatsapp.com/send?phone=+971564013448";
+      // console.log(whatsupoffers.href)
     }
   }
   
