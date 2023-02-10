71c9c3312622e0776ed7fea1bc7cf0c99c72581e
nabiljaved <nabeeljaved2016@gmail.com>
2023-02-10 18:06:52 +0400

    added mobile code

--- assets/js/whatsup.js
+++ assets/js/whatsup.js
@@ -1,22 +1,32 @@
   
   const whatsupA =  document.querySelector(".whatsup a");
   const whatsupoffers = document.querySelector(".whatsup_buttons a");
-  var x = window.matchMedia("(max-width: 786px)");
-  //console.log(whatsupoffers.href)
 
 
-  function myFunction(x) {
-    if (x.matches) { // If media query matches
-      whatsupA.href = "https://api.whatsapp.com/send?phone=+971564013448";
-      whatsupoffers.href = "https://api.whatsapp.com/send?phone=+971564013448" ;
-      console.log(whatsupoffers.href)
-    } else {
-      // // document.body.style.backgroundColor = "pink";
-      // whatsupA.href = "https://web.whatsapp.com/send?phone=+971564013448";
-      // whatsupoffers.href = "https://web.whatsapp.com/send?phone=+971564013448";
-      // console.log(whatsupoffers.href)
-    }
+
+  // let myMediaQuery = window.matchMedia('(max-width: 786px)');
+  // function widthChangeCallback(myMediaQuery) {
+  //   if(myMediaQuery.matches) {
+  //     console.log("mobile")
+  //     whatsupA.href = "https://api.whatsapp.com/send?phone=+971564013448";
+  //     whatsupoffers.href = "https://api.whatsapp.com/send?phone=+971564013448" ;
+  //     console.log(whatsupoffers.href)
+  //    } else {
+   
+  //    }
+  // }
+  // myMediaQuery.addEventListener('change', widthChangeCallback);
+
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // true for mobile device
+    console.log("mobile")
+    whatsupA.href = "https://api.whatsapp.com/send?phone=+971564013448";
+    whatsupoffers.href = "https://api.whatsapp.com/send?phone=+971564013448" ;
+  }else{
+    // false for not mobile device
+    document.write("not mobile device");
   }
+
+
+    
   
-  
-  myFunction(x) 
