a605833b15a8875e5c092f4776adfdc90d791e3d
nabiljaved <nabeeljaved2016@gmail.com>
2023-02-10 20:10:14 +0400

    fixed whts up

--- assets/js/whatsup.js
+++ assets/js/whatsup.js
@@ -1,9 +1,3 @@
-  
-  const whatsupA =  document.querySelector(".whatsup a");
-  const whatsupoffers = document.querySelector(".whatsup_buttons a");
-
-
-
   // let myMediaQuery = window.matchMedia('(max-width: 786px)');
   // function widthChangeCallback(myMediaQuery) {
   //   if(myMediaQuery.matches) {
@@ -18,13 +12,15 @@
   // myMediaQuery.addEventListener('change', widthChangeCallback);
 
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    // true for mobile device
-    console.log("mobile")
-    whatsupA.href = "https://api.whatsapp.com/send?phone=+971564013448";
-    whatsupoffers.href = "https://api.whatsapp.com/send?phone=+971564013448" ;
-  }else{
-    // false for not mobile device
-    document.write("not mobile device");
+      
+  const whatsupA =  document.querySelector(".whatsup a");
+  whatsupA.href = "https://api.whatsapp.com/send?phone=+971564013448";
+  const whatsupoffers = document.querySelectorAll(".whatsup_buttons a");
+    
+    whatsupoffers.forEach(function(element){
+      element.href = "https://api.whatsapp.com/send?phone=+971564013448";
+      
+    })
   }
 
 
